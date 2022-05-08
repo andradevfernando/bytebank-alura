4aba790a496c6f6c0eaa6909f2c532b94f75c22c
Beryan0 <andradevfernando@gmail.com>
2022-05-08 17:35:36 -0300

    Projeto bytebank alura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
+import { NovaTransferenciaComponent } from './nova-transferecia/nova-transferencia.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
